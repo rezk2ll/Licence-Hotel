e04de53758d606efa84bbe99ec9b59df03f60fb5
ayoub elkadhi <ayoub.el.kadhi@gmail.com>
2016-12-26 15:45:04 +0100

    pas encore terminé

---
+++ PROJET_HOTEL/detailsEnregistrement.aspx
@@ -0,0 +1,72 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/MasterPage.master" AutoEventWireup="true" CodeFile="detailsEnregistrement.aspx.cs" Inherits="detailsEnregistrement" %>
+
+
+<asp:Content ID="Content1" ContentPlaceHolderID="ContentPlaceHolder1" Runat="Server">
+    <form id="form1" runat="server">
+    <div class="container" style="padding-top: 6%">
+        <div class="col-md-10 col-md-offset-1 jumbotron">
+            <br />
+            <div class="panel panel-primary">
+                <div class="panel-heading">
+                                Informations de l'enregistrement              
+                </div>
+                <div id="infos" class="panel-body" runat="server">
+                    <asp:Table ID="Table1" runat="server">
+                        <asp:TableRow>
+                            <asp:TableCell>
+                                <asp:Label ID="Label1" runat="server" Text="Label">Nom et prenom : <br /></asp:Label>
+                            </asp:TableCell>
+                            <asp:TableCell>
+                                <asp:Label ID="nom" runat="server"></asp:Label>
+                            </asp:TableCell>
+                        </asp:TableRow>
+                        <asp:TableRow>
+                            <asp:TableCell>
+                                <asp:Label ID="Label3" runat="server" Text="Label">Cin : <br /></asp:Label> 
+                            </asp:TableCell>
+                            <asp:TableCell>
+                                <asp:Label ID="cin" runat="server"></asp:Label>
+                            </asp:TableCell>
+                        </asp:TableRow>
+                        <asp:TableRow>
+                            <asp:TableCell>
+                                <asp:Label ID="Label8" runat="server" Text="Label">Nombre de nuitées : <br /></asp:Label> 
+                            </asp:TableCell>
+                            <asp:TableCell>
+                                <asp:Label ID="nbrnuitees" runat="server"></asp:Label>
+                            </asp:TableCell>
+                        </asp:TableRow>
+                        <asp:TableRow>
+                            <asp:TableCell>
+                                <asp:Label ID="Label5" runat="server" Text="Label">Date Arrivée : <br /></asp:Label>
+                            </asp:TableCell>
+                            <asp:TableCell>
+                                <asp:Label ID="datearrivee" runat="server"></asp:Label>
+                            </asp:TableCell>
+                        </asp:TableRow>
+                        <asp:TableRow>
+                            <asp:TableCell>
+                                 <asp:Label ID="Label6" runat="server" Text="Label">Nombre de personnes : <br /></asp:Label>
+                            </asp:TableCell>
+                            <asp:TableCell>
+                                <asp:Label ID="nbrpersonnes" runat="server"></asp:Label>
+                            </asp:TableCell>
+                        </asp:TableRow>
+                        <asp:TableRow>
+                            <asp:TableCell>
+                                <asp:Label ID="Label7" runat="server" Text="Label">Formule : <br /></asp:Label>
+                            </asp:TableCell>
+                            <asp:TableCell>
+                                <asp:Label ID="formule" runat="server"></asp:Label>
+                            </asp:TableCell>
+                        </asp:TableRow>
+                    </asp:Table>
+                  
+                   
+                   <br /><br />
+                </div>
+            </div>
+        </div>
+    </div>
+    </form>
+    </asp:Content>
---
+++ PROJET_HOTEL/detailsEnregistrement.aspx.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.OleDb;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class detailsEnregistrement : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        if (!IsPostBack)
+        {
+            database db = new database();
+            OleDbConnection cnn = db.connection();
+            try
+            {
+                cnn.Open();
+                if (Request.Params["res"] != null)
+                {
+                    string slct = "SELECT * FROM reservation where id = " + Request.Params["res"] + " ORDER BY id DESC";
+                    OleDbCommand cmd = new OleDbCommand(slct, cnn);
+                    OleDbDataReader rd = cmd.ExecuteReader();
+                    if (rd != null)
+                    {
+                        rd.Read();
+                        nom.Text = (string)rd["nom"] +" " +(string)rd["prenom"];
+                        cin.Text = (string)rd["cin"];
+                        datearrivee.Text = (string)rd["date_arrivee"].ToString();
+                        nbrpersonnes.Text = rd["nombre"].ToString();
+                        if (rd["formule"].ToString() == "pd")
+                        {
+                            formule.Text = "Petit Déjeuner";
+                        }
+                        else if (rd["formule"].ToString() == "dp")
+                        {
+                            formule.Text = "Demi-Pension";
+                        }
+                        else
+                        {
+                            formule.Text = "Pension Complète";
+                        }
+                        nbrnuitees.Text = rd["nbnuits"].ToString();
+                        //string today = DateTime.Today.Date.ToString();
+                        //string[] date_comps = today.Split(' ');
+                        //string[] date1 = date_comps[0].Split('/');
+                        //string[] date2 = datearrivee.Text.Split('-');
+                        //string s = date1[0] + "=" + date1[1] + "=" + date1[2];
+                        //string y = date2[2] + "=" + date2[1] + "=" + date2[0];
+                        //if (s.Equals(y))
+                        //{
+                        //    Checkin.Visible = true;
+                        //    room.Visible = true;
+                        //    Label4.Visible = true;
+                        //    string str = "SELECT * FROM chambres where reservee like " + 0 + " and personnes like " + rd["nombre"].ToString() + " ORDER BY numero";
+                        //    OleDbCommand comm = new OleDbCommand(str, cnn);
+                        //    OleDbDataReader c = comm.ExecuteReader();
+                        //    if (c != null)
+                        //    {
+                        //        while (c.Read())
+                        //        {
+                        //            ListItem l = new ListItem();
+                        //            l.Text = c["numero"].ToString();
+                        //            l.Value = c["numero"].ToString();
+                        //            room.Items.Add(l);
+                        //        }
+                        //    }
+                        //}
+                        //else
+                        //{
+                        //    Checkin.Visible = false;
+                        //    room.Visible = false;
+                        //    Label4.Visible = false;
+                        //}
+
+                    }
+                    else
+                    {
+                        Response.Redirect("Default.aspx");
+                    }
+                }
+                else
+                {
+                    Response.Redirect("Default.aspx");
+                }
+
+            }
+            catch (Exception ex)
+            {
+                Response.Write(ex.Message);
+            }
+            cnn.Close();
+
+        }
+    }
+}
